8ce2d8fa9131d1fcc4e95831e48b92a6bd2999db
PiaoZhenJia <46585274+PiaoZhenJia@users.noreply.github.com>
2021-01-05 16:40:43 +0800

    Add files via upload

---
+++ AddConfig.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class AddConfigWindow : Window
+    {
+        public AddConfigWindow()
+        {
+            InitializeComponent();
+            RefreshTb();
+        }
+
+        private void BtnAdd(object sender, RoutedEventArgs e)
+        {
+            string res = CommonConfig.AddToFileHead(tbInputHead.Text, tbInputPerfix.Text);
+            RefreshTb();
+            MessageBox.Show(res,"添加结果");
+        }
+
+        private void BtnRefresh(object sender, RoutedEventArgs e)
+        {
+            CommonConfig.RefreshAll();
+            RefreshTb();
+        }
+
+        private void RefreshTb()
+        {
+            tbOutput.Text = CommonConfig.ShowKV();
+        }
+    }
+}
